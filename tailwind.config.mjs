f40ab748a7ec9a9d7a2a39adc00b8d3a153f5112
Daniel Steman <daniel-steman@live.nl>
2024-04-09 22:31:12 +0200

    added background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00CC99",
+        secondary: "#6600FF",
+      },
+    },
+  },
+  plugins: [],
+};
